5ee7c3f991519db2021e6d653969dd3f205fff5a
Danielgimp <57535036+Danielgimp@users.noreply.github.com>
2020-03-21 12:44:45 +0200

    Initial upload where Stage a is compelete
    The program compiles

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include "PhoneticFinder.hpp"
+using namespace std;
+
+namespace phonetic
+{
+    string find(string text,string word)
+    {
+        string str="This is Fucking Working!";
+        return str;
+    }
+}
+
+
