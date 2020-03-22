b23a84d85db7afd61ca133426f61644201fe67f7
Danielgimp <57535036+Danielgimp@users.noreply.github.com>
2020-03-22 12:38:31 +0200

    Added some basic functions

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -6,10 +6,27 @@ using namespace std;
 
 namespace phonetic
 {
+    
     string find(string text,string word)
     {
-        string str="This is Fucking Working!";
-        return str;
+        string tempText=text;         
+        if (text.find(word) != string::npos) { //locate word 
+            cout << text.substr(text.find(word),word.length()) << '\n';
+        }
+        for(size_t i=0;i<word.length();i++) //word lower case
+        {
+            char c=word.at(i);
+            word.at(i)=tolower(c);
+        }
+        for(size_t i=0;i<tempText.length();i++) //text lower case
+        {
+            char c=tempText.at(i);
+            tempText.at(i)=tolower(c);
+        }
+        if (tempText.find(word) != string::npos) { //locate word again
+            cout << text.substr(text.find(word),word.length()) << '\n';
+        }      
+        return "Not Yet Implemented";    
     }
 }
 
